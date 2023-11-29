571f6ca87be88c1f8a2e4b4e6548f7f947d22d3e
Mabsas <139257914+Mabsas@users.noreply.github.com>
2023-11-29 21:02:46 +0600

    fourth change

--- lib/main.dart
+++ lib/main.dart
@@ -72,7 +72,7 @@ class _HomePage extends State<HomePage> {
                       filled: true,
                       fillColor: const Color.fromARGB(230, 201, 196, 236),
                       prefixIcon: const Icon(Icons.search),
-                      hintText: "Looking for something?",
+                      hintText: "Looking for anything?",
                       hintStyle: const TextStyle(fontStyle: FontStyle.italic),
                       border: OutlineInputBorder(
                           borderRadius: BorderRadius.circular(50.0),
